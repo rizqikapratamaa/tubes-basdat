ce460f42fd0a4920e1ec16df85bfd4b95ce29bb3
Rinnearu <rayhanridhar@gmail.com>
2024-05-06 11:21:16 +0700

    update: added produk.sql

---
+++ produk.sql
@@ -0,0 +1,58 @@
+create table produk (
+    id_produk VARCHAR(8) PRIMARY KEY NOT NULL,
+    email_pembuat VARCHAR(55),
+    judul_produk TEXT,
+    genre VARCHAR(28),
+    tanggal_rilis DATE,
+    tipe_produk VARCHAR(6)
+);
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT05392', 'rcluettj@imageshack.us', 'aenean fermentum donec ut mauris', 'Bluegrass', '7/10/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT55904', 'bmacgaughey9@wordpress.com', 'vitae nisi nam', 'Techno', '9/4/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT73397', 'jlyptrade6@google.it', 'amet cursus id turpis integer aliquet', 'Rock', '3/29/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT14364', 'epegrumh@e-recht24.de', 'ante nulla justo', 'Punk', '10/9/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT39979', 'avaughng@dagondesign.com', 'mauris lacinia', 'Alternative Rock', '5/8/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT88123', 'acettell1@salon.com', 'lectus aliquam sit amet diam', 'Reggae', '5/9/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT48569', 'lpaudind@abc.net.au', 'penatibus et magnis', 'Dubstep', '5/5/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT64164', 'bmacgaughey9@wordpress.com', 'eget elit sodales', 'Ska', '11/14/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT51509', 'avaughng@dagondesign.com', 'non sodales sed tincidunt eu', 'Grunge', '5/16/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT08500', 'lmerigonb@admin.ch', 'lacus at', 'Metal', '12/29/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT98895', 'avaughng@dagondesign.com', 'hac habitasse platea dictumst maecenas', 'Electronic Dance Music (EDM)', '9/29/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT11491', 'lmerigonb@admin.ch', 'posuere felis sed', 'Opera', '1/25/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT54310', 'bmacgaughey9@wordpress.com', 'ut odio', 'Rock', '11/14/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT11025', 'lpaudind@abc.net.au', 'bibendum morbi non quam nec', 'Metal', '9/20/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT04709', 'rcluettj@imageshack.us', 'fusce', 'Disco', '10/16/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT66936', 'lpaudind@abc.net.au', 'velit donec diam neque vestibulum eget vulputate', 'Dubstep', '1/30/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT77371', 'acettell1@salon.com', 'lacinia aenean sit', 'House', '9/12/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT62162', 'lpaudind@abc.net.au', 'nulla dapibus dolor vel est donec', 'Funk', '3/5/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT09461', 'acettell1@salon.com', 'semper', 'R&B/Soul', '4/21/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT16662', 'bmacgaughey9@wordpress.com', 'ut massa quis augue luctus tincidunt nulla', 'New Wave', '3/21/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT97115', 'epegrumh@e-recht24.de', 'ligula', 'Gospel', '2/17/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT45447', 'lmerigonb@admin.ch', 'quis odio consequat varius integer ac leo', 'Jazz', '5/26/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT39995', 'avaughng@dagondesign.com', 'semper porta', 'Grunge', '2/11/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT70859', 'wromanf@ihg.com', 'mauris', 'Grunge', '10/18/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT86026', 'jlyptrade6@google.it', 'viverra diam vitae quam suspendisse potenti', 'Hip-Hop/Rap', '2/8/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT00267', 'acettell1@salon.com', 'vitae ipsum', 'Classical', '9/22/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT04054', 'jlyptrade6@google.it', 'sed tristique in tempus', 'Funk', '12/2/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT14401', 'avaughng@dagondesign.com', 'montes nascetur', 'Opera', '4/7/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT85985', 'lmerigonb@admin.ch', 'orci mauris lacinia', 'Latin', '7/30/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT71282', 'lpaudind@abc.net.au', 'lobortis ligula sit', 'New Wave', '3/14/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT13398', 'lpaudind@abc.net.au', 'vulputate vitae nisl', 'Country', '5/9/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT34766', 'lpaudind@abc.net.au', 'metus arcu', 'Opera', '9/20/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT89147', 'jlyptrade6@google.it', 'porttitor lorem id ligula suspendisse ornare', 'Blues', '5/9/2023', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT45339', 'wromanf@ihg.com', 'hendrerit', 'Latin', '2/2/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT26678', 'lmerigonb@admin.ch', 'at diam nam', 'New Wave', '8/21/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT75497', 'lpaudind@abc.net.au', 'justo maecenas rhoncus aliquam', 'Disco', '2/26/2024', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT64685', 'epegrumh@e-recht24.de', 'vulputate vitae nisl aenean lectus pellentesque eget', 'Trance', '4/1/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT94493', 'jlyptrade6@google.it', 'non mattis pulvinar nulla', 'Funk', '1/10/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT79796', 'epegrumh@e-recht24.de', 'quis lectus suspendisse potenti in eleifend quam', 'Jazz', '1/28/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT07081', 'nluffman0@theglobeandmail.com', 'lobortis convallis tortor risus dapibus augue vel', 'Reggae', '8/25/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT35508', 'avaughng@dagondesign.com', 'id consequat in consequat ut nulla sed', 'Blues', '3/22/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT15976', 'acettell1@salon.com', 'semper sapien a libero nam dui', 'Pop', '1/25/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT78677', 'avaughng@dagondesign.com', 'sapien cursus vestibulum proin eu', 'Punk', '3/5/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT85093', 'acettell1@salon.com', 'nulla sed vel enim sit', 'Drum and Bass', '2/13/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT39144', 'bmacgaughey9@wordpress.com', 'vestibulum ante ipsum primis in', 'Dubstep', '12/9/2023', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT52762', 'nluffman0@theglobeandmail.com', 'ligula nec sem', 'Rock', '6/23/2023', 'Single');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT94827', 'nluffman0@theglobeandmail.com', 'amet', 'Bluegrass', '4/5/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT50627', 'epegrumh@e-recht24.de', 'quam pede', 'Opera', '1/3/2024', 'EP');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT29796', 'rcluettj@imageshack.us', 'cum sociis natoque penatibus', 'Gospel', '3/5/2024', 'Album');
+insert into produk (id_produk, email_pembuat, judul_produk, genre, tanggal_rilis, tipe_produk) values ('PDT08556', 'epegrumh@e-recht24.de', 'eget tincidunt eget tempus vel pede', 'Blues', '6/10/2023', 'Single');
