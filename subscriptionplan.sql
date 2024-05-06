77bf307b781c6fe4a503e78d2b562ca7e91f0dc5
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-05-06 09:27:53 +0700

    add: video ekstra

--- subscriptionplan.sql
+++ subscriptionplan.sql
@@ -2,6 +2,6 @@ create table subscriptionplan (
 	jenis_langganan VARCHAR(10) PRIMARY KEY NOT NULL,
 	harga_bulanan VARCHAR(5)
 );
-insert into MOCK_DATA (jenis_langganan, harga_bulanan) values ('Pelajar', 35000);
-insert into MOCK_DATA (jenis_langganan, harga_bulanan) values ('Perorangan', 55000);
-insert into MOCK_DATA (jenis_langganan, harga_bulanan) values ('Keluarga', 85000);
+insert into subscriptionplan (jenis_langganan, harga_bulanan) values ('Pelajar', 35000);
+insert into subscriptionplan (jenis_langganan, harga_bulanan) values ('Perorangan', 55000);
+insert into subscriptionplan (jenis_langganan, harga_bulanan) values ('Keluarga', 85000);
